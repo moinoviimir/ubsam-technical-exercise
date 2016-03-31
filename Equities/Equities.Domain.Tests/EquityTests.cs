56e648a92c5f6df15b905df8130ef62499ec488f
Mir <moinoviimir@gmail.com>
2016-03-31 07:42:03 +0300

    Added StockTypes.

---
+++ Equities/Equities.Domain.Tests/EquityTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Equities.Domain.Tests
+{
+    [TestFixture]
+    public class EquityTests
+    {
+        [TestCase]
+        public void EquityHasCorrectStockType()
+        {
+            var sut = new Equity(2.0m, 2);
+            Assert.AreEqual(TypeOfStock.Equity, sut.StockType);
+        }
+    }
+}
