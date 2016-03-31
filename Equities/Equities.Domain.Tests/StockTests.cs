90992ae00f7a0a5fa8dd3dfab659cabe72becdef
Mir <moinoviimir@gmail.com>
2016-03-31 06:11:37 +0300

    Added Stock constructor tests

---
+++ Equities/Equities.Domain.Tests/StockTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+
+namespace Equities.Domain.Tests
+{
+    [TestFixture]
+    public class StockTests
+    {
+        [TestCase]
+        public void CtorCorrectlyAssignsPrice()
+        {
+            var price = 1.0m;
+            var sut = new Mock<Stock>(MockBehavior.Strict, price, 0);
+            Assert.AreEqual(sut.Object.Price, price);
+        }
+
+        [TestCase]
+        public void CtorCorrectlyAssignsQuantity()
+        {
+            var quantity = 1;
+            var sut = new Mock<Stock>(MockBehavior.Strict, 1.0m, quantity);
+            Assert.AreEqual(sut.Object.Quantity, quantity);
+        }
+    }
+}
