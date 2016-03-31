34f5f9154ba7b6c2b5586e6a3fa150889d9c022a
Mir <moinoviimir@gmail.com>
2016-03-31 04:57:35 +0300

    Initial test added.

---
+++ Equities/Equities.Domain/Fund.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equities.Domain
+{
+    public class Fund
+    { 
+        public void Add(Stock item)
+        {
+
+        }
+
+        public bool Contains(Stock item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
