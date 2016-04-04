cf69c7554dac0be85cba3f521adf4898fdc8c14b
Mir <moinoviimir@gmail.com>
2016-04-04 19:23:00 +0300

    a couple files got lost somehow

---
+++ Equities/Equities.Domain.Tests/BondTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace Equities.Domain.Tests
+{
+    [TestFixture]
+    public class BondTests
+    {
+        [TestCase]
+        public void NewBondHasCorrectStockType()
+        {
+            var sut = new Bond(1.0m, 1);
+            Assert.AreEqual(TypeOfStock.Bond, sut.StockType);
+        }
+
+        [TestCase]
+        public void BondTransactionCostIsZeroIfZeroQuantityBought()
+        {
+            var sut = new Bond(1.0m, 0);
+            Assert.AreEqual(0, sut.TransactionCost);
+        }
+
+        [TestCase]
+        public void BondTransactionCostIsZeroIfStockCostsZero()
+        {
+            var sut = new Bond(0m, 10);
+            Assert.AreEqual(0, sut.TransactionCost);
+        }
+
+        [TestCase]
+        public void BondTransactionCostIsCorrectForNonzeroQuantityAndCost()
+        {
+            var sut = new Bond(10.0m, 5);
+            Assert.AreEqual(1, sut.TransactionCost);
+        }
+    }
+}
---
+++ Equities/Equities/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
+using Equities.Builders;
+using Equities.Domain;
+using Equities.Models;
+
+namespace Equities.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ICollectionView Stocks { get; private set; }
+        public ReadOnlyObservableCollection<SummaryModel> Summary { get; private set; }
+
+        public ICollectionView StockTypes { get; }
+
+        private Fund _fund;
+
+
+        public MainWindowViewModel(Fund fund)
+        {
+            _fund = fund;
+
+            UpdateStocks();
+
+            
+
+            StockTypes = new ListCollectionView(new List<string> {TypeOfStock.Equity.ToString(), TypeOfStock.Bond.ToString()});
+        }
+
+        private void UpdateStocks()
+        {
+            var list = _fund.GetStocks().Select(stock => new StockViewModel(stock)).ToList();
+            Stocks = new ListCollectionView(list);
+
+            var summaryList =
+                new SummaryBuilder(_fund)
+                    .WithEquities()
+                    .WithBonds()
+                    .WithTotal()
+                    .Summary;
+
+            Summary =
+                new ReadOnlyObservableCollection<SummaryModel>(
+                    new ObservableCollection<SummaryModel>(summaryList));
+        }
+
+        public void AddToFund(Stock stock)
+        {
+            _fund.Add(stock);
+            UpdateStocks();
+        }
+
+        
+
+        
+            
+    }
+}
